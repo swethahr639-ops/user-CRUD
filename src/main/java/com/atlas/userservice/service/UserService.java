eebe0cf01ee4a2556f43db8b5276e64e73ae6503
Pandu <pandu123@gmail.com>
2026-04-24 06:59:42 +0530

    scrum-8 adding service interface user

---
+++ src/main/java/com/atlas/userservice/service/UserService.java
@@ -0,0 +1,25 @@
+package com.atlas.userservice.service;
+/*
+ * Service interface for User Operations 
+ */
+
+import java.util.List;
+
+import com.atlas.userservice.service.dto.CommonResponseDTO;
+import com.atlas.userservice.service.dto.UserRequestDTO;
+import com.atlas.userservice.service.dto.UserResponseDTO;
+
+public interface UserService {
+	public CommonResponseDTO<Long> saveUser(UserRequestDTO userRequestDTO);
+
+	public CommonResponseDTO<UserResponseDTO> getUser(Long userId);
+
+	public CommonResponseDTO<List<UserResponseDTO>> getUsers();
+
+	public CommonResponseDTO<UserResponseDTO> updateUser(Long userID, UserResponseDTO userResponseDTO);
+
+	public CommonResponseDTO<Void> deletUser(Long userId);
+
+	public CommonResponseDTO<UserResponseDTO> getUser(String emailId, String password);
+
+}
